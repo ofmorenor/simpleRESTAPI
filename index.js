25c5cb6926276fa92c5c460a47a1aed58344e2c7
OscarMoreno <oscar.moreno@mvm.com.co>
2020-11-13 08:31:17 -0500

    install and run express

---
+++ index.js
@@ -0,0 +1,13 @@
+(function main(){
+    const express = require('express');
+    const app = express();
+    const port = 3000;
+
+    app.get('/', (req, res) => {
+        res.send('hello express');
+    })
+
+    app.listen(port, () => {
+        console.log(`restapi listening in port ${port}...`);
+    })
+})();
